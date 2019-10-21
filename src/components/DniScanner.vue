c913216d47b3de258990acc75bda6a018ae050bd
Alejandro Steinmetz <asteinmetz78@gmail.com>
2019-10-21 10:05:36 -0300

    Initial commit

---
+++ src/components/DniScanner.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>Dni scanner</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      video: {},
+      canvas: {},
+      captures: []
+    }
+  },
+  mounted () {
+    this.video = this.$refs.video
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        this.video.src = window.URL.createObjectURL(stream)
+        this.video.play()
+      })
+    }
+  },
+  methods: {
+    capture () {
+      this.canvas = this.$refs.canvas
+      // var context = this.canvas.getContext('2d').drawImage(this.video, 0, 0, 640, 480)
+      this.captures.push(this.canvas.toDataURL('image/png'))
+    }
+  }
+}
+</script>
+
+<style>
+    #video {
+        background-color: #000000;
+    }
+    #canvas {
+        display: none;
+    }
+    li {
+        display: inline;
+        padding: 5px;
+    }
+</style>
